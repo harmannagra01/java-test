f2206cf42fa2e8cb52437940579e03cbe59520ab
harmanjit.nagra <harmanjit.nagra@accenture.com>
2017-09-04 00:10:40 -0700

    initial commit

---
+++ src/Rectangle.java
@@ -0,0 +1,18 @@
+public class Rectangle {
+  public int length;
+  public int width;
+
+  public Rectanle(int length, int width) {
+    this.length = length;
+    this.width = width;
+  }
+
+  public int getArea(){
+    return length * width;
+  }
+
+  public int getPerimeter(){
+    return 2 * (length + width);
+  }
+
+}
